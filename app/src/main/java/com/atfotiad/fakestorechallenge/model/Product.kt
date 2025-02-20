daca0fd2e18186a4323ca61d1a84381d6c03e619
atfotiad <thfotiad@gmail.com>
2025-02-21 01:16:34 +0200

    Add Product model

---
+++ app/src/main/java/com/atfotiad/fakestorechallenge/model/Product.kt
@@ -0,0 +1,15 @@
+package com.atfotiad.fakestorechallenge.model
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class Product(
+    val id: Int,
+    val title: String,
+    val price: Double,
+    val description: String,
+    val category: String,
+    @Json(name = "image") val imageUrl: String
+
+)
