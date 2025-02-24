2b536dfc9b2ef2b3bfd794fff10161c00c3782f2
atfotiad <thfotiad@gmail.com>
2025-02-24 19:18:55 +0200

    Use viewDataBinding delegate to simplify viewBinding

--- app/src/main/java/com/atfotiad/fakestorechallenge/MainActivity.kt
+++ app/src/main/java/com/atfotiad/fakestorechallenge/MainActivity.kt
@@ -5,12 +5,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import com.atfotiad.fakestorechallenge.databinding.ActivityMainBinding
 import com.atfotiad.fakestorechallenge.security.TokenManager
+import com.atfotiad.fakestorechallenge.utils.ui.viewDataBinding
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -19,20 +19,19 @@ import javax.inject.Inject
 class MainActivity : AppCompatActivity() {
     @Inject
     lateinit var tokenManager: TokenManager
+    private val binding by viewDataBinding(ActivityMainBinding::inflate)
 
     private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        val binding: ActivityMainBinding =
-            DataBindingUtil.setContentView(this, R.layout.activity_main)
+        navController = findNavController(R.id.nav_host_fragment)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        navController = findNavController(binding.navHostFragment.id)
 
         lifecycleScope.launch {
             if (tokenManager.hasToken()) {
