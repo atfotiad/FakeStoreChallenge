cc89b35d1341c727c86b2b893e4e114709182a00
atfotiad <thfotiad@gmail.com>
2025-02-24 12:52:26 +0200

    Add FakeStoreUseCase abstraction

---
+++ app/src/main/java/com/atfotiad/fakestorechallenge/usecase/FakeStoreUseCase.kt
@@ -0,0 +1,15 @@
+package com.atfotiad.fakestorechallenge.usecase
+
+import com.atfotiad.fakestorechallenge.model.LoginResponse
+import com.atfotiad.fakestorechallenge.model.Product
+import com.atfotiad.fakestorechallenge.utils.repository.Result
+
+interface FakeStoreUseCase {
+    suspend fun getCategories(): List<String>
+    suspend fun getProductsByCategory(category: String): List<Product>
+    suspend fun login(username: String, password: String): Result<LoginResponse>
+    suspend fun updateProduct(product: Product): Boolean
+    suspend fun partiallyUpdateProduct(product: Product): Boolean
+
+
+}
---
+++ app/src/main/java/com/atfotiad/fakestorechallenge/usecase/FakeStoreUseCaseImpl.kt
@@ -0,0 +1,34 @@
+package com.atfotiad.fakestorechallenge.usecase
+
+import com.atfotiad.fakestorechallenge.data.repository.FakeStoreRepository
+import com.atfotiad.fakestorechallenge.model.LoginResponse
+import com.atfotiad.fakestorechallenge.model.Product
+import com.atfotiad.fakestorechallenge.utils.repository.Result
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class FakeStoreUseCaseImpl @Inject constructor(
+    private val repository: FakeStoreRepository
+) : FakeStoreUseCase{
+    override suspend fun getCategories(): List<String> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getProductsByCategory(category: String): List<Product> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun login(username: String, password: String): Result<LoginResponse> {
+        return repository.login(username, password)
+    }
+
+    override suspend fun updateProduct(product: Product): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun partiallyUpdateProduct(product: Product): Boolean {
+        TODO("Not yet implemented")
+    }
+
+}
