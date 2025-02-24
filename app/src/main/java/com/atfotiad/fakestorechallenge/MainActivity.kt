5f9d8f5b81a5329dfc1f1f0959128c47573397ad
atfotiad <thfotiad@gmail.com>
2025-02-24 18:07:58 +0200

    Add logic to navigate based on Token existence

--- app/src/main/java/com/atfotiad/fakestorechallenge/MainActivity.kt
+++ app/src/main/java/com/atfotiad/fakestorechallenge/MainActivity.kt
@@ -6,19 +6,40 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import com.atfotiad.fakestorechallenge.databinding.ActivityMainBinding
+import com.atfotiad.fakestorechallenge.security.TokenManager
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    @Inject
+    lateinit var tokenManager: TokenManager
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        val binding: ActivityMainBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        navController = findNavController(binding.navHostFragment.id)
+
+        lifecycleScope.launch {
+            if (tokenManager.hasToken()) {
+                navController.navigate(R.id.homeFragment)
+            } else {
+                navController.navigate(R.id.loginFragment)
+            }
         }
     }
 }
