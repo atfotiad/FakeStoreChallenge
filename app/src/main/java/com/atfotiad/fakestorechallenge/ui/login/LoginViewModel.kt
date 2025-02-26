b82d9ca798f16214ced55951879e99b7a7ef3476
atfotiad <thfotiad@gmail.com>
2025-02-26 19:24:07 +0200

    Update LoginViewModel to handle login errors

--- app/src/main/java/com/atfotiad/fakestorechallenge/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/atfotiad/fakestorechallenge/ui/login/LoginViewModel.kt
@@ -1,15 +1,15 @@
 package com.atfotiad.fakestorechallenge.ui.login
 
-import android.content.Context
 import android.util.Patterns
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
+import com.atfotiad.fakestorechallenge.data.model.LoginRequest
+import com.atfotiad.fakestorechallenge.security.TokenManager
 import com.atfotiad.fakestorechallenge.usecase.FakeStoreUseCase
-import com.atfotiad.fakestorechallenge.utils.repository.RepoUtils.getOrError
+import com.atfotiad.fakestorechallenge.utils.repository.Result
 import dagger.hilt.android.lifecycle.HiltViewModel
-import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -21,22 +21,29 @@ import javax.inject.Inject
 @HiltViewModel
 class LoginViewModel @Inject constructor(
     private val useCase: FakeStoreUseCase,
-    @ApplicationContext private val context: Context
+    private val tokenManager: TokenManager
 ) : ViewModel() {
-    private val _loginState = MutableStateFlow<LoginState>(LoginState.Idle)
-    val loginState = _loginState.asStateFlow()
 
+    private val _uiState = MutableStateFlow(LoginUiState())
+    val uiState: StateFlow<LoginUiState> = _uiState.asStateFlow()
 
-    val _username = MutableStateFlow("")
-    private val username: StateFlow<String> = _username.asStateFlow()
-
-    val _password = MutableStateFlow("")
-    private val password: StateFlow<String> = _password.asStateFlow()
+    val username: MutableStateFlow<String> = MutableStateFlow("")
+    val password: MutableStateFlow<String> = MutableStateFlow("")
 
     private val _isLoginEnabled = MutableStateFlow(false)
     val isLoginEnabled: LiveData<Boolean> = _isLoginEnabled.asStateFlow().asLiveData()
 
     init {
+        validateForm()
+    }
+
+    fun onUsernameChanged(newUsername: String) {
+        username.value = newUsername
+        validateForm()
+    }
+
+    fun onPasswordChanged(newPassword: String) {
+        password.value = newPassword
         validateForm()
     }
 
@@ -49,7 +56,15 @@ class LoginViewModel @Inject constructor(
                     username.isNotBlank()
                 }
                 val isPasswordValid =
-                    password.isNotBlank() && password.trim().length >= 6 // Example: At least 6 characters
+                    password.isNotBlank() && password.trim().length >= 6
+                _uiState.update {
+                    it.copy(
+                        username = username,
+                        password = password,
+                        isUsernameValid = isUsernameValid,
+                        isPasswordValid = isPasswordValid
+                    )
+                }
                 isUsernameValid && isPasswordValid
             }.collect { isValid ->
                 _isLoginEnabled.update { isValid }
@@ -57,20 +72,30 @@ class LoginViewModel @Inject constructor(
         }
     }
 
-    fun login(username: String, password: String) {
+    fun login() {
         viewModelScope.launch {
-            _loginState.value = LoginState.Loading
-            val loginResponse = useCase.login(username, password).getOrError(context)
-            if (loginResponse != null) {
-                _loginState.value = LoginState.Success(loginResponse.token)
-            } else {
-                _loginState.value = LoginState.Idle
+            _uiState.update { it.copy(isLoading = true, isSuccess = false, errorMessage = null) }
+            when (val result = useCase.login(LoginRequest(username.value, password.value))) {
+                is Result.Success -> {
+                    tokenManager.saveToken(result.data.token)
+                    _uiState.update { it.copy(isLoading = false, isSuccess = true) }
+                }
+
+                is Result.Error -> {
+                    _uiState.update {
+                        it.copy(
+                            isLoading = false,
+                            errorMessage = result.exception.localizedMessage
+                        )
+                    }
+                }
+
             }
         }
     }
 
-    fun resetState() {
-        _loginState.value = LoginState.Idle
+    fun clearErrorMessage() {
+        _uiState.update { it.copy(errorMessage = null) }
     }
 
 }
