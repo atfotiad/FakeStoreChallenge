57c9a3c25444e25656e0b949f3711d5ec0f9a5c1
atfotiad <thfotiad@gmail.com>
2025-02-26 19:21:57 +0200

    Remove unnecessary LoginState data class

--- app/src/main/java/com/atfotiad/fakestorechallenge/ui/login/LoginState.kt
+++
@@ -1,8 +0,0 @@
-package com.atfotiad.fakestorechallenge.ui.login
-
-sealed class LoginState {
-    data object Idle : LoginState()
-    data object Loading : LoginState()
-    data class Success(val token: String) : LoginState()
-    data class Error(val exception: Exception) : LoginState()
-}
