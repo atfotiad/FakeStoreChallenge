cf444f374d4d3e321dfd175543f217f1fb924aff
atfotiad <thfotiad@gmail.com>
2025-02-26 20:43:17 +0200

    Implement remaining functionality

--- app/src/main/java/com/atfotiad/fakestorechallenge/usecase/FakeStoreUseCaseImpl.kt
+++ app/src/main/java/com/atfotiad/fakestorechallenge/usecase/FakeStoreUseCaseImpl.kt
@@ -1,8 +1,9 @@
 package com.atfotiad.fakestorechallenge.usecase
 
+import com.atfotiad.fakestorechallenge.data.model.LoginRequest
 import com.atfotiad.fakestorechallenge.data.repository.FakeStoreRepository
 import com.atfotiad.fakestorechallenge.data.model.LoginResponse
-import com.atfotiad.fakestorechallenge.data.model.Product
+import com.atfotiad.fakestorechallenge.data.model.product.Product
 import com.atfotiad.fakestorechallenge.utils.repository.Result
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -11,24 +12,28 @@ import javax.inject.Singleton
 class FakeStoreUseCaseImpl @Inject constructor(
     private val repository: FakeStoreRepository
 ) : FakeStoreUseCase{
-    override suspend fun getCategories(): List<String> {
-        TODO("Not yet implemented")
+    override suspend fun getCategories(): Result<List<String>> {
+        return repository.getCategories()
     }
 
     override suspend fun getProductsByCategory(category: String): List<Product> {
         TODO("Not yet implemented")
     }
 
-    override suspend fun login(username: String, password: String): Result<LoginResponse> {
-        return repository.login(username, password)
+    override suspend fun login(loginRequest: LoginRequest): Result<LoginResponse> {
+        return repository.login(loginRequest.username, loginRequest.password)
     }
 
-    override suspend fun updateProduct(product: Product): Boolean {
-        TODO("Not yet implemented")
+    override suspend fun updateProduct(product: Product): Result<Unit> {
+        return repository.updateProduct(product.id, product)
     }
 
-    override suspend fun partiallyUpdateProduct(product: Product): Boolean {
-        TODO("Not yet implemented")
+    override suspend fun partiallyUpdateProduct(product: Product): Result<Unit> {
+      return repository.partiallyUpdateProduct(product.id, product)
+    }
+
+    override suspend fun getAllProducts(): Result<List<Product>> {
+        return repository.getAllProducts()
     }
 
 }
