0ca08c48484cde810fd8c8616f17d181f14d05c0
atfotiad <thfotiad@gmail.com>
2025-02-26 20:24:38 +0200

    Update cryptography functionality

--- app/src/main/java/com/atfotiad/fakestorechallenge/security/CryptoManager.kt
+++ app/src/main/java/com/atfotiad/fakestorechallenge/security/CryptoManager.kt
@@ -1,19 +1,18 @@
 package com.atfotiad.fakestorechallenge.security
 
-import android.content.Context
 import android.security.keystore.KeyGenParameterSpec
 import android.security.keystore.KeyProperties
-import dagger.hilt.android.qualifiers.ApplicationContext
+import android.util.Log
 import java.security.KeyStore
 import javax.crypto.Cipher
 import javax.crypto.KeyGenerator
 import javax.crypto.SecretKey
 import javax.crypto.spec.IvParameterSpec
 import javax.inject.Inject
+/** Class that encrypts and decrypts login token using AES
+ * */
+class CryptoManager @Inject constructor() {
 
-class CryptoManager @Inject constructor(
-    @ApplicationContext private val context: Context
-) {
     private val keyStore = KeyStore.getInstance("AndroidKeyStore").apply {
         load(null)
     }
@@ -50,15 +49,21 @@ class CryptoManager @Inject constructor(
     }
 
     fun encrypt(data: String): String {
+        Log.d("CryptoManager", "encrypt: String before encryption: $data")
         val bytes = encryptCipher.doFinal(data.toByteArray())
-        return "${bytes.toHexString()}:${encryptCipher.iv.toHexString()}"
+        val encryptedString = "${bytes.toHexString()}:${encryptCipher.iv.toHexString()}"
+        Log.d("CryptoManager", "encrypt: String after encryption: $encryptedString")
+        return encryptedString
     }
 
     fun decrypt(data: String): String {
+        Log.d("CryptoManager", "decrypt: String before decryption: $data")
         val (bytes, iv) = data.split(":").let {
             it[0].hexToByteArray() to it[1].hexToByteArray()
         }
-        return getDecryptCipherForIv(iv).doFinal(bytes).toString(Charsets.UTF_8)
+        val decryptedString = getDecryptCipherForIv(iv).doFinal(bytes).toString(Charsets.UTF_8)
+        Log.d("CryptoManager", "decrypt: String after decryption: $decryptedString")
+        return decryptedString
     }
 
     companion object {
