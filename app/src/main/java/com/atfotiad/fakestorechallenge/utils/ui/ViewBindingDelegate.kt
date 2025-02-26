1a0463a63d2b2b0c8e06a8028ee9322efe7c39c5
atfotiad <thfotiad@gmail.com>
2025-02-26 20:06:38 +0200

    Update viewBinding delegate to simplify DataBinding in Fragments

--- app/src/main/java/com/atfotiad/fakestorechallenge/utils/ui/ViewBindingDelegate.kt
+++ app/src/main/java/com/atfotiad/fakestorechallenge/utils/ui/ViewBindingDelegate.kt
@@ -1,27 +1,34 @@
 package com.atfotiad.fakestorechallenge.utils.ui
 
 import android.view.LayoutInflater
-import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
+import android.view.View
+import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.Observer
 import androidx.viewbinding.ViewBinding
 import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KProperty
 
-inline fun <reified T : ViewBinding> AppCompatActivity.viewDataBinding() =
+inline fun <reified T : ViewBinding> FragmentActivity.viewDataBinding() =
     ActivityViewBindingDelegate(T::class.java, this)
 
-inline fun <reified T : ViewBinding> Fragment.viewDataBinding() =
-    FragmentViewBindingDelegate(T::class.java, this)
+inline fun <reified T : ViewDataBinding> Fragment.viewDataBinding(
+    noinline viewDataBindingFactory: (View) -> T
+) =
+    FragmentViewDataBindingDelegate(this, viewDataBindingFactory)
 
 class ActivityViewBindingDelegate<T : ViewBinding>(
     private val bindingClass: Class<T>,
-    private val activity: AppCompatActivity,
-) : ReadOnlyProperty<AppCompatActivity, T> {
+    private val activity: FragmentActivity,
+) : ReadOnlyProperty<FragmentActivity, T> {
 
     private var binding: T? = null
 
-    override fun getValue(thisRef: AppCompatActivity, property: KProperty<*>): T {
+    override fun getValue(thisRef: FragmentActivity, property: KProperty<*>): T {
         binding?.let { return it }
 
         val inflateMethod = bindingClass.getMethod("inflate", LayoutInflater::class.java)
@@ -32,35 +39,47 @@ class ActivityViewBindingDelegate<T : ViewBinding>(
     }
 }
 
-class FragmentViewBindingDelegate<T : ViewBinding>(
-    private val bindingClass: Class<T>,
+class FragmentViewDataBindingDelegate<T : ViewBinding>(
     val fragment: Fragment,
+    val viewDataBindingFactory: (View) -> T
 ) : ReadOnlyProperty<Fragment, T> {
-
     private var binding: T? = null
 
     init {
-        fragment.viewLifecycleOwnerLiveData.observe(fragment) { viewLifecycleOwner ->
-            viewLifecycleOwner.lifecycle.addObserver(object :
-                androidx.lifecycle.DefaultLifecycleObserver {
-                override fun onDestroy(owner: androidx.lifecycle.LifecycleOwner) {
-                    binding = null
+        fragment.lifecycle.addObserver(object : DefaultLifecycleObserver {
+            val viewLifecycleOwnerLiveDataObserver =
+                Observer<LifecycleOwner?> {
+                    val viewLifecycleOwner = it ?: return@Observer
+                    viewLifecycleOwner.lifecycle.addObserver(object : DefaultLifecycleObserver {
+                        override fun onDestroy(owner: LifecycleOwner) {
+                            binding = null
+                        }
+                    })
                 }
-            })
-        }
+
+            override fun onCreate(owner: LifecycleOwner) {
+                fragment.viewLifecycleOwnerLiveData.observeForever(
+                    viewLifecycleOwnerLiveDataObserver
+                )
+            }
+
+            override fun onDestroy(owner: LifecycleOwner) {
+                fragment.viewLifecycleOwnerLiveData.removeObserver(
+                    viewLifecycleOwnerLiveDataObserver
+                )
+            }
+        })
     }
 
     override fun getValue(thisRef: Fragment, property: KProperty<*>): T {
         binding?.let { return it }
+        val lifecycle = fragment.viewLifecycleOwner.lifecycle
+        if (!lifecycle.currentState.isAtLeast(Lifecycle.State.INITIALIZED)) {
+            throw IllegalStateException("Should not attempt to get bindings when Fragment views are destroyed.")
+        }
+        return viewDataBindingFactory(thisRef.requireView()).also {
+            this.binding = it
+        }
 
-        val inflateMethod = bindingClass.getMethod(
-            "inflate",
-            LayoutInflater::class.java,
-            ViewGroup::class.java,
-            Boolean::class.java
-        )
-        val invokedBinding = inflateMethod.invoke(null, thisRef.layoutInflater, null, false) as T
-        binding = invokedBinding
-        return binding!!
     }
 }
