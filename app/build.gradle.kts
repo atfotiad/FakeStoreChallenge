7bd50adbcddfe5475bc33ca6eaea7e780e3e7acd
atfotiad <thfotiad@gmail.com>
2025-02-26 06:21:57 +0200

    Add Glide dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,6 +64,8 @@ dependencies {
     implementation(libs.converter.moshi)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    ksp("com.github.bumptech.glide:compiler:4.16.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
