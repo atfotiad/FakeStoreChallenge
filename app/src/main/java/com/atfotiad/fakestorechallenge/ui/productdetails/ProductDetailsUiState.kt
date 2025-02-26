1fecf7b9ca8566265f9c95b305affbaa2ce27a94
atfotiad <thfotiad@gmail.com>
2025-02-26 20:51:06 +0200

    Add ProductDetailsUiState data class

---
+++ app/src/main/java/com/atfotiad/fakestorechallenge/ui/productdetails/ProductDetailsUiState.kt
@@ -0,0 +1,7 @@
+package com.atfotiad.fakestorechallenge.ui.productdetails
+
+import com.atfotiad.fakestorechallenge.data.model.product.Product
+
+data class ProductDetailsUiState(
+    val product: Product? = null
+)
