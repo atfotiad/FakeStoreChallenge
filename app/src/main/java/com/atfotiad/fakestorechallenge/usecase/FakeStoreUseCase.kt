e09fc73908d81b7199a68cb4aed526406925277f
atfotiad <thfotiad@gmail.com>
2025-02-26 20:42:03 +0200

    Update to wrap interface methods with Result<T>.

--- app/src/main/java/com/atfotiad/fakestorechallenge/usecase/FakeStoreUseCase.kt
+++ app/src/main/java/com/atfotiad/fakestorechallenge/usecase/FakeStoreUseCase.kt
@@ -1,15 +1,17 @@
 package com.atfotiad.fakestorechallenge.usecase
 
+import com.atfotiad.fakestorechallenge.data.model.LoginRequest
 import com.atfotiad.fakestorechallenge.data.model.LoginResponse
-import com.atfotiad.fakestorechallenge.data.model.Product
+import com.atfotiad.fakestorechallenge.data.model.product.Product
 import com.atfotiad.fakestorechallenge.utils.repository.Result
 
 interface FakeStoreUseCase {
-    suspend fun getCategories(): List<String>
+    suspend fun getCategories(): Result<List<String>>
     suspend fun getProductsByCategory(category: String): List<Product>
-    suspend fun login(username: String, password: String): Result<LoginResponse>
-    suspend fun updateProduct(product: Product): Boolean
-    suspend fun partiallyUpdateProduct(product: Product): Boolean
+    suspend fun login(loginRequest: LoginRequest): Result<LoginResponse>
+    suspend fun updateProduct(product: Product): Result<Unit>
+    suspend fun partiallyUpdateProduct(product: Product): Result<Unit>
+    suspend fun getAllProducts(): Result<List<Product>>
 
 
 }
